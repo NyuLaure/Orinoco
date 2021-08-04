3a14a3d358daff5b3e957519e0619b5395843cce
Laure <laure03.09@gmail.com>
2021-08-04 16:08:18 +0200

    essai_1_Page

--- frontend/produits.js
+++ frontend/produits.js
@@ -1,14 +1,29 @@
-let response;
-let itemIdlist = [];
+var global = [{"lenses":["35mm 1.4","50mm 1.6"],"_id":"5be1ed3f1c9d44000030b061","name":"Zurss 50S","price":49900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/vcam_1.jpg"},{"lenses":["50mm 1.8","60mm 2.8","24-60mm 2.8/4.5"],"_id":"5be1ef211c9d44000030b062","name":"Hirsch 400DTS","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","price":309900,"imageUrl":"http://localhost:3000/images/vcam_2.jpg"},{"lenses":["25mm 4.5"],"_id":"5be9bc241c9d440000a730e7","name":"Franck JS 105","price":209900,"description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","imageUrl":"http://localhost:3000/images/vcam_3.jpg"},{"lenses":["50mm 1.7","35mm 1.4"],"_id":"5be9c4471c9d440000a730e8","name":"Kuros TTS","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","price":159900,"imageUrl":"http://localhost:3000/images/vcam_4.jpg"},{"lenses":["50mm 1.4","35mm 1.8","28-200mm 2.8/4.5"],"_id":"5be9c4c71c9d440000a730e9","name":"Katatone","description":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.","price":59900,"imageUrl":"http://localhost:3000/images/vcam_5.jpg"}]
 
-fetch("http://localhost:3000/api/cameras").then(function(reponse) {
-    return response.json();
-}).then(function (obj) {
-    let productSection = document.getElementById("liste-produits");
+console.log(global[0]._id, global[1].price);
 
-    for (let i = 0; i < obj.length; i++ ) {
+var item = document.getElementById("items");
 
-        const productCard = 
-        <div class="card col-sm-12 col-md-6 col-lg-4" sty
-    }
-})
+// item.innerHTML=global[0]._id + global[0].price + global[0].name;
+
+//for ( let i=0; i<global.length; i++){
+   // item.innerHTML+="<p>" + global[i].name + "</p>" ;
+  //  item.innerHTML+="<p>" + global[i].price/100 +",00€" + "</p>" ;
+  //  item.innerHTML+='<img class="image" src="' + global[i].imageUrl + '"/>';
+  //  item.innerHTML+="<p>" + global[i].description + "</p>" ;
+//  }
+
+
+
+for ( let i=0; i<global.length; i++){
+    let products = `
+        <div class="col-sm-12 col-md-6 col-lg-4 mb-5 mt-5 text-center " >
+            <p>${global[i].name} </p> 
+            <p>${global[i].price/100},00€ </p> 
+            <img class="image" src="${global[i].imageUrl}"/>
+            <p>${global[i].description}</p> 
+        </div>
+    `; 
+    item.innerHTML+=products;
+}
+
